2a3dc15b7442cab7e95066f3bd048d5c82ea5cff
Angad88 <121978333+Angad88@users.noreply.github.com>
2023-07-29 10:37:05 -0700

    Q23

---
+++ Final/Final/ConcuricGenericQueue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final
+{
+    internal class ConcuricGenericQueue<T>
+    {
+        private T[] queue;
+        private int frontVar;
+        private int backVar;
+        private int length;
+
+        public ConcuricGenericQueue(int maxLength)
+        {
+            queue = new T[maxLength];
+            frontVar = 0;
+            backVar = -1;
+            length = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            lock (queue)
+            {
+                if (length < queue.Length)
+                {
+                    backVar = (backVar + 1) % queue.Length;
+                    queue[backVar] = item;
+                    length++;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Queue is full.");
+                }
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (queue)
+            {
+                if (length > 0)
+                {
+                    T item = queue[frontVar];
+                    frontVar = (frontVar + 1) % queue.Length;
+                    length--;
+                    return item;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Queue is empty.");
+                }
+            }
+        }
+
+        public int GetCurrentLength()
+        {
+            lock (queue)
+            {
+                return length;
+            }
+        }
+
+        public T Peek()
+        {
+            lock (queue)
+            {
+                if (length > 0)
+                {
+                    return queue[frontVar];
+                }
+                else
+                {
+                    throw new InvalidOperationException("Queue is empty.");
+                }
+            }
+        }
+    }
+}
---
+++ Final/Final/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Final
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcuricGenericQueue<int> queue = new ConcuricGenericQueue<int>(5);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Console.WriteLine("The Peek: " + queue.Peek());
+            Console.WriteLine("Length: " + queue.GetCurrentLength());
+
+            int firstItem = queue.Dequeue();
+            int secondItem = queue.Dequeue();
+
+            Console.WriteLine("Length: " + queue.GetCurrentLength());
+            Console.WriteLine("The Peek: " + firstItem + ", " + secondItem);
+        }
+    }
+}
